445d6fc7391448e0c3c056e0a2d2c988625c214e
Aravind-medi-14 <aravindmedi1234@gmail.com>
2025-03-20 10:44:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import App1 from "./components/App1";
+import App2 from "./components/App2";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Link to="app1">App1</Link>
+        <Link to="app2">App2</Link>
+      </div>
+      <hr />
+      <div>
+        <Routes>
+          <Route index element={<App1 />}></Route>
+          <Route path="app1" element={<App1 />}></Route>
+          <Route path="app2" element={<App2 />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/App1.jsx
@@ -0,0 +1,3 @@
+export default function App1() {
+  return <h1>THis is App1</h1>;
+}
